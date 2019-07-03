b5eb2b0e8e0b55cba261c32a3a4a91806c4c8b55
andrewleesteele <andrewleesteele@gmail.com>
2019-07-02 20:24:15 -0700

    correct formatting

--- TrayLCController.js
+++ TrayLCController.js
@@ -1,7 +1,7 @@
 ({
-	doInit : function(component, event, helper) {
+    doInit : function(component, event, helper) {
         /*
-		var action = component.get("c.getItems");
+        var action = component.get("c.getItems");
         action.setParams({recordId : component.get("v.recordId")});
         action.setCallback(this, function(response){
             var state = response.getState();
@@ -15,12 +15,31 @@
         
         $A.enqueueAction(action);
         */
-        console.log(helper.thirdpartyClientCaller(component, event, helper)); 
+        helper.thirdpartyClientCaller(component, event, helper); 
 
-	},
+    },
     stageChange : function(component, event) {
-		console.log("numItems has changed");
+        console.log("numItems has changed");
 
         doInit(component, event);
+    },
+    incrementPage : function(component, event, helper) {
+        var new_page_number = component.get("v.page_number")+1;
+        var items = component.get("v.items");
+        
+        if(items.length != 0){
+            component.set("v.page_number", new_page_number);
+            helper.thirdpartyClientCaller(component, event, helper)  
+        }
+        console.log(component.get("v.page_number"));
+    },
+    decrementPage : function(component, event, helper) {
+        var new_page_number = component.get("v.page_number")-1;
+        if(new_page_number > 0){
+            component.set("v.page_number", new_page_number);
+            helper.thirdpartyClientCaller(component, event, helper)
+        }      
+        console.log(component.get("v.page_number"));
+
     }
  })
--- TrayLCHelper.js
+++ TrayLCHelper.js
@@ -1,26 +1,23 @@
 ({ 
     thirdpartyClientCaller : function(component, event, helper) { 
-    	var xmlHttp = new XMLHttpRequest();
-    	var url = 'https://eb39c2bb-15bf-47f7-8361-44b915c43047.trayapp.io';
-    	xmlHttp.open( "GET", url, true );
-	    xmlHttp.setRequestHeader('Content-Type', 'application/json');
-        xmlHttp.setRequestHeader('Accept', '12345');
-        xmlHttp.setRequestHeader('Content-Language', 1);
-		xmlHttp.responseType = 'text';
         
-		xmlHttp.onload = function () {
-    	    console.log("onload");
-        	console.log(xmlHttp.readyState);
-        	console.log(xmlHttp.status);
-    		if (xmlHttp.readyState === 4) {
-    	    	if (xmlHttp.status === 200) {
-	            	console.log(xmlHttp.response['data']);
-            		console.log(xmlHttp.responseText);
-                    component.set("v.items",['Andrew', 'Steele', 'Awesome']);
-		        }
-	    	}
-		};
-
-	    xmlHttp.send( null );
+        var xmlHttp = new XMLHttpRequest();
+        var url = 'https://eb39c2bb-15bf-47f7-8361-44b915c43047.trayapp.io';
+        
+        xmlHttp.open( "GET", url, true );
+        xmlHttp.setRequestHeader('Content-Type', 'application/json');
+        xmlHttp.setRequestHeader('Accept', component.get("v.recordId"));
+        xmlHttp.setRequestHeader('Content-Language', component.get("v.page_number"));
+        xmlHttp.responseType = 'text';
+        xmlHttp.onload = function () {
+            console.log("onload");
+            if (xmlHttp.readyState === 4) {
+                if (xmlHttp.status === 200) {
+                    var response = JSON.parse(xmlHttp.responseText);
+                    component.set("v.items",response.data);
+                }
+            }
+        };
+        xmlHttp.send( null );
     }
 })
